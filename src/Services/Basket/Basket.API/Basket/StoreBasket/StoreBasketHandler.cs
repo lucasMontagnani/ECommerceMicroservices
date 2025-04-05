23cbc5ec316bbc0f91da9231b44f3ab2eb5b72c9
Lucas Montagnani <lucascalilelias@yahoo.com.br>
2025-04-05 18:20:47 -0300

    FEAT: Basket Microservices with Vertical Slice Architecture and CQRS

---
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -0,0 +1,29 @@
+﻿namespace Basket.API.Basket.StoreBasket
+{
+    public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+    public record StoreBasketResult(string UserName);
+
+    public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
+    {
+        public StoreBasketCommandValidator()
+        {
+            RuleFor(x => x.Cart).NotEmpty().WithMessage("The Shopping Cart can not be null");
+            RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("The UserName is required");
+        }
+    }
+
+    public class StoreBasketCommandHandler(IBasketRepository repository) : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+    {
+        public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+        {
+            ShoppingCart shoppingCart = command.Cart;
+
+            // Store basket in database (use Marten upsert: if exist = update, if not = create)
+            await repository.StoreBasket(shoppingCart, cancellationToken);
+
+            // TODO: Update cache
+
+            return new StoreBasketResult(command.Cart.UserName);
+        }
+    }
+}
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,8 +1,7 @@
-using HealthChecks.UI.Client;
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
-
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container
+
+builder.Services.AddCarter();
 
 var assembly = typeof(Program).Assembly;
 builder.Services.AddMediatR(config =>
@@ -15,22 +14,16 @@ builder.Services.AddMediatR(config =>
     config.AddOpenBehavior(typeof(LogginBehavior<,>));
 });
 
-builder.Services.AddValidatorsFromAssembly(assembly);
-
-builder.Services.AddCarter();
-
 builder.Services.AddMarten(opts =>
 {
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    // To set/specify the identity field of the entity as username
+    opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
 }).UseLightweightSessions();
-
-if (builder.Environment.IsDevelopment())
-    builder.Services.InitializeMartenWith<CatalogInitialData>();
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
-builder.Services.AddHealthChecks()
-                    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 
 var app = builder.Build();
 // Configure the HTTP request pipeline
@@ -38,11 +31,5 @@ var app = builder.Build();
 app.MapCarter();
 
 app.UseExceptionHandler(options => { });
-
-app.UseHealthChecks("/health",
-    new HealthCheckOptions
-    {
-        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-    });
 
 app.Run();
