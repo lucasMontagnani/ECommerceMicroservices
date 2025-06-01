a8f4b8ad130948cabe0c82ca5a4fd1690ce5607e
Lucas Montagnani <lucascalilelias@yahoo.com.br>
2025-06-01 00:00:51 -0300

    FEAT: Building API Gateways with Yarp Reverse Proxy applying Gateway Routing Pattern

---
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -0,0 +1,25 @@
+// Add services to the container.
+using Microsoft.AspNetCore.RateLimiting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddRateLimiter(rateLimiterOptions =>
+{
+    rateLimiterOptions.AddFixedWindowLimiter("fixed", options =>
+    {
+        options.Window = TimeSpan.FromSeconds(10);
+        options.PermitLimit = 5;
+    });
+});
+
+// Configure the HTTP request pipeline.
+var app = builder.Build();
+
+app.UseRateLimiter();
+
+app.MapReverseProxy();
+
+app.Run();
